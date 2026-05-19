3fe0a229375fdcf2d24f2493269044413cb2abdf
Kauan <kauanferreiraa11@gmail.com>
2026-05-19 10:26:30 -0300

    editor int avaliado

--- ed/prin_list1/src/go/main.go
+++ ed/prin_list1/src/go/main.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 // mostra a lista com o elemento sword destacado
 func ToStr(l *DList[int], sword *DNode[int]) string {
-	return ""
+	string := "["
+	for it := l.root.next; it != l.root; it = it.next {
+		if it == sword {
+			string += fmt.Sprintf(" %d>", it.Value)
+		} else {
+			string += fmt.Sprintf(" %d", it.Value)
+		}
+	}
+	string += " ]"
+	return fmt.Sprint(string)
 }
 
 // move para frente na lista circular
 func Next(l *DList[int], it *DNode[int]) *DNode[int] {
-	return nil
+	next := it.next
+	if next == it.root {
+		next = next.next
+	}
+	return next
 }
 
 func main() {
 	var qtd, chosen int
 	fmt.Scan(&qtd, &chosen)
-	fmt.Println(qtd, chosen)
 	l := NewDList[int]()
 	for i := 1; i <= qtd; i++ {
 		l.PushBack(i)
