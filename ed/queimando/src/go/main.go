ffc29cabb06efadf725e7cfa7a37b5e6e0dd3d55
Kauan <kauanferreiraa11@gmail.com>
2026-06-08 11:26:17 -0300

    queimando com pilha resolvido

--- ed/queimando/src/go/main.go
+++ ed/queimando/src/go/main.go
@@ -6,10 +6,38 @@ import (
 	"os"
 )
 
+type Pos struct {
+	l, c int
+}
+
+func getNeighbors(l, c int) [][]int {
+	return [][]int{
+		{l - 1, c},
+		{l + 1, c},
+		{l, c - 1},
+		{l, c + 1},
+	}
+}
+
 func burnTrees(grid [][]rune, l, c int) {
 	stack := NewStack[Pos]()
-	_ , _ , _ = mat, l, c
+	stack.Push(Pos{l, c})
 
+	for !stack.IsEmpty() {
+		pos := stack.Pop()
+
+		if pos.l < 0 || pos.l >= len(grid) || pos.c < 0 || pos.c >= len(grid[0]) {
+			continue
+		}
+		if grid[pos.l][pos.c] != '#' {
+			continue
+		}
+		grid[pos.l][pos.c] = 'o'
+
+		for _, neighbor := range getNeighbors(pos.l, pos.c) {
+			stack.Push(Pos{neighbor[0], neighbor[1]})
+		}
+	}
 	// Essa função deve usar uma list como pilha
 	// e marcar as árvores na matriz como queimados
 	// Uma sugestão de como fazer isso é:
---
+++ ed/subset_sum/treinando/go/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func tostrforreversed(vet []int) string {
+	return "[" + tostrrev(vet) + "]"
+}
+
+func tostrfornormal(vet []int) string {
+	return "[" + tostr(vet) + "]"
+}
+func tostr(vet []int) string {
+	if len(vet) == 0 {
+		return ""
+	}
+	vetStr := make([]string, len(vet))
+	vetStr[0] = strconv.Itoa(vet[0])
+	if len(vet) > 1 {
+		return vetStr[0] + ", " + tostr(vet[1:])
+	}
+
+	return vetStr[0]
+}
+
+func tostrrev(vet []int) string {
+	if len(vet) == 0 {
+		return ""
+	}
+	vetStr := make([]string, len(vet))
+	vetStr[0] = strconv.Itoa(vet[len(vet)-1])
+	if len(vet) > 1 {
+		return vetStr[0] + ", " + tostrrev(vet[:len(vet)-1])
+	}
+
+	return vetStr[0]
+}
+
+// reverse: inverte os elementos do slice
+func reverse(vet []int) {
+	if len(vet) <= 1 {
+		return
+	}
+	vet[0], vet[len(vet)-1] = vet[len(vet)-1], vet[0]
+	reverse(vet[1 : len(vet)-1])
+}
+
+// sum: soma dos elementos do slice
+func sum(vet []int) int {
+	if len(vet) == 0 {
+		return 0
+	}
+	return vet[0] + sum(vet[1:])
+}
+
+// mult: produto dos elementos do slice
+func mult(vet []int) int {
+	if len(vet) == 0 {
+		return 1
+	}
+	return vet[0] * mult(vet[1:])
+}
+
+// min: retorna o índice e valor do menor valor
+// crie uma função recursiva interna do modelo
+// var rec func(v []int) (int, int)
+// para fazer uma recursão que retorna valor e índice
+func min(vet []int) int {
+	if len(vet) == 0 {
+		return -1
+	}
+	var rec func(v []int) (int, int)
+	idx, val := 0, vet[0]
+	rec = func(v []int) (int, int) {
+		if len(v) == 1 {
+			return 0, v[0]
+		}
+		idx, val = rec(v[1:])
+		if v[0] < val {
+			return 0, v[0]
+		}
+		return idx + 1, val
+	}
+	idx, val = rec(vet)
+	return idx
+}
+
+func main() {
+	var vet []int
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		if !scanner.Scan() {
+			break
+		}
+		line := scanner.Text()
+		args := strings.Fields(line)
+		fmt.Println("$" + line)
+
+		switch args[0] {
+		case "end":
+			return
+		case "read":
+			vet = nil
+			for _, arg := range args[1:] {
+				if val, err := strconv.Atoi(arg); err == nil {
+					vet = append(vet, val)
+				}
+			}
+		case "tostr":
+			fmt.Println(tostrfornormal(vet))
+		case "torev":
+			fmt.Println(tostrforreversed(vet))
+		case "reverse":
+			reverse(vet)
+		case "sum":
+			fmt.Println(sum(vet))
+		case "mult":
+			fmt.Println(mult(vet))
+		case "min":
+			fmt.Println(min(vet))
+		default:
+			fmt.Println("fail: comando invalido")
+		}
+	}
+}
