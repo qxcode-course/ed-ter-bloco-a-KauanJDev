e13f51e467aefbfe879d6c71749434952ec73d5f
Kauan <kauanferreiraa11@gmail.com>
2026-06-17 13:09:19 -0300

    resolvendo BstRemove

--- ed/bst_remove/src/go/main.go
+++ ed/bst_remove/src/go/main.go
@@ -14,16 +14,75 @@ type Node struct {
 	Right *Node
 }
 
+func (node *Node) insert(value int) {
+	if value == node.Value {
+		return
+	}
+	if value < node.Value {
+		if node.Left == nil {
+			node.Left = &Node{Value: value}
+		} else {
+			node.Left.insert(value)
+		}
+	} else {
+		if node.Right == nil {
+			node.Right = &Node{Value: value}
+		} else {
+			node.Right.insert(value)
+		}
+	}
+}
+
 func BstInsert(values []int) *Node {
-	// TODO
-	_ = values
-	return nil
+	if len(values) == 0 {
+		return nil
+	}
+	node := &Node{Value: values[0]}
+	for _, value := range values[1:] {
+		node.insert(value)
+	}
+	return node
+}
+
+func MaiorDireita(node *Node) *Node {
+	if node.Right != nil {
+		return MaiorDireita(node.Right)
+	} else {
+		aux := node
+		if node.Left != nil {
+			node = node.Left
+		} else {
+			node = nil
+		}
+		return aux
+	}
+}
+func MenorEsquerda(node *Node) *Node {
+	if node.Left != nil {
+		return MenorEsquerda(node.Left)
+	} else {
+		aux := node
+		if node.Right != nil {
+			node = node.Right
+		} else {
+			node = nil
+		}
+		return aux
+	}
 }
 
 func BstRemove(node *Node, value int) *Node {
-	// TODO
-	_, _ = node, value
-	return nil
+	if node == nil {
+		return nil
+	}
+	if value < node.Value {
+		BstRemove(node.Left, value)
+	} else {
+		if value > node.Value {
+			BstRemove(node.Right, value)
+		}
+	}
+	return node
 }
 
 // -----------------------------------------------------------------------------------
